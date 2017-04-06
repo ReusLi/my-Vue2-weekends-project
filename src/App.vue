2b0cbff74f50c8cf40a1aed373531b627844defb
reusLi <574399934@qq.com>
2017-04-06 23:15:54 +0800

    init project.

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <List></List>
+  </div>
+</template>
+
+<script>
+  import List from './components/lists/List'
+
+  export default {
+    name: 'app',
+    components: {
+      List
+    }
+  }
+</script>
+
+<style>
+  /*#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }*/
+  body{
+    background-color: #324057;
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ src/components/lists/List.vue
@@ -0,0 +1,76 @@
+<template>
+  <el-row :gutter="10">
+    <el-col :xs="8" :sm="8" :md="8" :lg="8">
+      <div class="main-list">
+        <el-card v-for="card in cards" class="box-card">
+        {{card}}
+        </el-card>
+      </div>
+    </el-col>
+
+    <el-col :xs="8" :sm="8" :md="8" :lg="8">
+      <div class="main-area">
+        <el-input
+          class="main-area-input"
+          placeholder="请输入内容"
+          @keyup.enter.native="pushCards"
+          v-model="area"/>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  import ListItem from './List-item'
+
+  export default {
+    name: 'List',
+    components: {
+      ListItem
+    },
+    data () {
+      return {
+        area: '',
+        cards: []
+      }
+    },
+    methods: {
+      pushCards: function () {
+        console.log('asss')
+        this.cards.push(this.area)
+        this.area = ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .main-list {
+    border: 1px solid #E5E9F2;
+    /*background-color: #13CE66;*/
+    /*opacity: .5;*/
+    height: 600px;
+    padding: 0 10%;
+  }
+
+  .main-area {
+    border: 1px solid #E5E9F2;
+    opacity: .8;
+    height: 600px;
+    padding: 0 10%;
+  }
+
+  .main-area-input {
+  	min-height: 36px;
+  	width: 80%;
+  	border-radius: 5px;
+  	border: 1px solid #E5E9F2;
+    margin-top: 30%;
+  }
+
+  .box-card {
+    margin-top: 10%;
+    background-color: #475669;
+    color: #ffffff;
+  }
+</style>
