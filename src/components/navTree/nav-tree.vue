a5118620524e3122a14810b7d59ab033eecb7c91
reusLi <574399934@qq.com>
2017-04-10 23:59:26 +0800

    require json file in components

--- src/components/navTree/nav-tree.vue
+++ src/components/navTree/nav-tree.vue
@@ -6,47 +6,13 @@
 </template>
 
 <script>
-  //  import json from '../../json/navTree/tree'
+  var treeJson = require('../../json/nav-tree/tree.json')
 
   export default {
     name: 'navTree',
     data () {
       return {
-        data: [{
-          label: '一级 1',
-          children: [{
-            label: '二级 1-1',
-            children: [{
-              label: '三级 1-1-1'
-            }]
-          }]
-        }, {
-          label: '一级 2',
-          children: [{
-            label: '二级 2-1',
-            children: [{
-              label: '三级 2-1-1'
-            }]
-          }, {
-            label: '二级 2-2',
-            children: [{
-              label: '三级 2-2-1'
-            }]
-          }]
-        }, {
-          label: '一级 3',
-          children: [{
-            label: '二级 3-1',
-            children: [{
-              label: '三级 3-1-1'
-            }]
-          }, {
-            label: '二级 3-2',
-            children: [{
-              label: '三级 3-2-1'
-            }]
-          }]
-        }],
+        data: treeJson,
         defaultProps: {
           label: 'label',
           children: 'children'
