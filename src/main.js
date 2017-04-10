e04a7e9abef7a3f5a6071907f5e97d95b2d9aa9c
reusLi <574399934@qq.com>
2017-04-10 20:31:23 +0800

    commit main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import App from './App'
 
 // import router from './router'
-import 'element-ui/lib/theme-default/index.css'
+// import 'element-ui/lib/theme-default/index.css'
+import '../theme/index.css'
 import ElementUI from 'element-ui'
 
 Vue.config.productionTip = false
